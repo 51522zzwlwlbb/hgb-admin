4ea250da20eec792d5a7e0c71c4f5c7509b2cc64
炒饭大师干饭达人恋爱废柴健身呆头鹅 <20981116+51522zzwlwlbb@users.noreply.github.com>
2023-09-18 15:01:44 +0800

    init: 示例项目

--- src/config/website.config.ts
+++
@@ -1,9 +0,0 @@
-import logoImage from '@/assets/images/logo.png';
-import loginImage from '@/assets/images/account-logo.png';
-
-export const websiteConfig = Object.freeze({
-  title: 'HGB-admin',
-  logo: logoImage,
-  loginImage: loginImage,
-  loginDesc: 'hgb admin 中后台前端/设计解决方案',
-});
